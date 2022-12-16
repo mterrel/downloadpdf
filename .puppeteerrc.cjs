a3c1c6694b9cc689f0f50fa1954e15908e784125
Mark Terrel <mark@adaptable.io>
2022-12-16 08:26:56 -0700

    Update pptr config

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -1,9 +1,10 @@
 const {join} = require('path');
+process.env.npm_config_loglevel = "notice";
 
 /**
  * @type {import("puppeteer").Configuration}
  */
 module.exports = {
   // Changes the cache location for Puppeteer.
-  cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
+  cacheDirectory: join('/layers/paketo-buildpacks_npm-install/modules/node_modules', '.cache', 'puppeteer'),
 };
